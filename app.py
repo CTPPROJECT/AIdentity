1cbea7df567934cc5ea1019a72622a9fd4bb0d26
shahks-hub <kasaf.shah28@myhunter.cuny.edu>
2023-11-06 18:18:44 -0500

    working

--- app.py
+++ app.py
@@ -1,19 +1,29 @@
 import dash
 from dash import dcc, html
 from dash.dependencies import Input, Output
-from tabs import demographic
 import dash_bootstrap_components as dbc
-from tabs import demographic, geographic, psycographic
+import pandas as pd
+import plotly.express as px
 
-app = dash.Dash(__name__)
+
+app = dash.Dash(__name__, suppress_callback_exceptions=True)
+
+#COMBINED DATA SECTION
+###################################################
+#add all data here and initilize with you initials
+csv_file_K = 'data/LocalPayNYC.csv'
+data_visualize_K = pd.read_csv(csv_file_K)
+
+###################################################
+#DATA SECTION ENDS
 
 app.layout = html.Div([
     html.H1("Careers influenced by various factors over the years"),
     
-    dcc.Tabs(id='tabs', value='tab-1', children=[
-        dcc.Tab(label='Geographic', value='tab-1'),
-        dcc.Tab(label='Psycographic', value='tab-2'),
-        dcc.Tab(label='Demographic', value='tab-3'),
+    dcc.Tabs(id='tabs', value='geographic', children=[
+        dcc.Tab(label='Geographic', value='geographic'),
+        dcc.Tab(label='Psycographic', value='psycographic'),
+        dcc.Tab(label='Demographic', value='demographic'),
     ]),
     
     html.Div(id='tab-content'),
@@ -21,18 +31,76 @@ app.layout = html.Div([
 
 @app.callback(
     Output('tab-content', 'children'),
-    Input('tabs', 'value'),
+    Input('tabs', 'value')
 )
 def render_content(tab):
-    if tab == 'tab-1':
+#HENRY'S SECTION
+###################################################################################################
+    if tab == 'geographic':
+        # Return the content for the "Geographic" tab
+        return html.Div("Content for Geographic tab")
+
+#BRENDAN'S SECTION
+####################################################################################################
+    elif tab == 'psycographic':
+        # Return the content for the "Psycographic" tab
+        return html.Div("Content for Psycographic tab")
+
+#KASAF'S SECTION
+#####################################################################################################
+    elif tab == 'demographic':
+        # Return the content for the "Demographic" tab
         
-        return geographic.layout
-    elif tab == 'tab-2':
-       
-        return psycographic.layout
-    elif tab == 'tab-3':
-       
-        return demographic.layout
+        return dbc.Container([
+            dbc.Row([
+                dbc.Col([
+                    dcc.Dropdown(
+                        id='comparison-factor',
+                        options=[
+                            {'label': 'Gender', 'value': 'gender'},
+                            {'label': 'Ethnicity', 'value': 'ethnicity'},
+                            {'label': 'Race', 'value': 'race'}
+                        ],
+                        value='gender',  # Default selection
+                    ),
+                ], width=3),
+            ]),
+            dbc.Row([
+                dbc.Col([
+                    dcc.Graph(id='comparison-chart-box'),
+                ], width=6),
+                dbc.Col([
+                    dcc.Graph(id='comparison-chart-bar'),
+                ], width=6),
+            ]),
+        ])
+
+@app.callback(
+    [Output('comparison-chart-box', 'figure'),
+     Output('comparison-chart-bar', 'figure')],
+    Input('comparison-factor', 'value')
+)
+def update_plots(selected_factor):
+    fig_box = px.box(
+        data_visualize_K,
+        x=selected_factor,
+        y="upper_pay_band_bound",
+        title=f"Pay Distribution by {selected_factor}",
+    )
+
+    fig_bar = px.histogram(
+        data_visualize_K,
+        x=selected_factor,
+        color="job_category",
+        title=f"Distribution of Job Categories by {selected_factor}",
+    )
+
+    return fig_box, fig_bar
+
+
+###########################################################################################
+#Kasaf's section ends here
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
--- tabs/geographic.py
+++
@@ -1,5 +0,0 @@
-# tab1.py
-from dash import html
-
-
-layout = html.Div("This is the content of Geographic Tab")
